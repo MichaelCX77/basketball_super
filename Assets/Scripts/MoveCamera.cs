0d081b1cf180ca87a7fb772e0d75e012d65ead83
Joana <joana.ads@outlook.com>
2022-05-16 21:24:40 -0300

    Ajustes do projeto

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -7,15 +7,15 @@ public class MoveCamera : MonoBehaviour
     // Update is called once per frame
     [SerializeField] private Transform limiteD, limiteE, bola;
     void Update() {
-        if(GameManager.instance.jogoComecou == true){
-            if(bola == null && GameManager.instance.bolaEmCena > 0){
-                bola = GameObject.Find("Bola").GetComponent<Transform>();
-            }
-        } else if (GameManager.instance.bolaEmCena > 0){
-            Vector3 posCam = transform.position;
-            posCam.x = bola.position.x;
-            posCam.x = Mathf.Clamp(posCam.x, limiteE.position.x, limiteD.position.x);
-            transform.position = posCam;
-        }
+        // if(GameManager.instance.jogoComecou == true){
+        //     if(bola == null && GameManager.instance.bolaEmCena > 0){
+        //         bola = GameObject.Find("Bola").GetComponent<Transform>();
+        //     }
+        // } else if (GameManager.instance.bolaEmCena > 0){
+        //     Vector3 posCam = transform.position;
+        //     posCam.x = bola.position.x;
+        //     posCam.x = Mathf.Clamp(posCam.x, limiteE.position.x, limiteD.position.x);
+        //     transform.position = posCam;
+        // }
     }
 }
